24891ee74beb0a7cb80e6b56eb3f4cecd1a8b0c4
Volodymyr <klischvolodimir@gmail.com>
2019-09-29 16:22:18 +0300

    sda

---
+++ seventh_task.js
@@ -0,0 +1,11 @@
+let res = 9;
+
+
+if(res === 0 ){
+    console.log('нуль');
+}
+else if (res !== 2 || res !== 4 || res !== 6 || res !== 8) {
+    console.log('число непарне');
+} else if(res !== 1 || res !== 3 || res !== 5 || res !== 7 || res !== 9) {
+    console.log('число парне');
+}
---
+++ sixth.js
@@ -0,0 +1,12 @@
+let a = 'Оля';
+let res = a.toLowerCase();
+
+
+if( res === 'андрій' || res === 'назар' || res === 'сергій'){
+    console.log('чоловіче ім`я');
+}
+else if (res === 'настя' || res === 'оля' || res === 'юля') {
+    console.log('жіноче ім`я');
+} else {
+    console.log('хз');
+}
